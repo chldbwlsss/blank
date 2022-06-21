17da46c90ee738cdfdfb16e572578a3143aa0d6c
hypercubist <58693701+hypercubist@users.noreply.github.com>
2022-06-21 12:52:50 +0900

    [fix] userapi import

--- src/main/java/ddobab/blank/web/UserApiV1Controller.java
+++ src/main/java/ddobab/blank/web/UserApiV1Controller.java
@@ -1,9 +1,12 @@
 package ddobab.blank.web;
 
 
+import ddobab.blank.security.dto.SessionUserDto;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @Slf4j
 @RequestMapping("/api/v1/user")
@@ -11,10 +14,8 @@ import org.springframework.web.bind.annotation.RestController;
 public class UserApiV1Controller {
 
     @GetMapping
-    public ResponseEntity<SessionUserDto> getSessionUser(@CookieValue("JSESSIONID") String sessionId, @SessionAttribute(name="loginUser", required = false) SessionUserDto loginUser) {
+    public ResponseEntity<SessionUserDto> getSessionUser(@SessionAttribute(name="loginUser", required = false) SessionUserDto loginUser) {
         log.info("[GET] LOGIN-USER : {}", loginUser);
-        log.info("SESSEION-ID : {}", sessionId);
-        return new ResponseEntity<>(loginUser, loginUser!=null?HttpStatus.OK:HttpStatus.NO_CONTENT);
+        return new ResponseEntity<>(loginUser, loginUser!=null? HttpStatus.OK:HttpStatus.NO_CONTENT);
     }
-
 }
