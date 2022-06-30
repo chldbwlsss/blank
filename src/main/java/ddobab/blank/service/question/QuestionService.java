be4d418d44d588351e7baebbf24ce43a7539a513
hypercubist <58693701+hypercubist@users.noreply.github.com>
2022-06-30 17:42:04 +0900

    [add] add custom ex

--- src/main/java/ddobab/blank/service/question/QuestionService.java
+++ src/main/java/ddobab/blank/service/question/QuestionService.java
@@ -14,6 +14,7 @@ import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.LocalTime;
 import java.util.List;
+import java.util.NoSuchElementException;
 import java.util.stream.Collectors;
 
 @RequiredArgsConstructor
@@ -27,21 +28,21 @@ public class QuestionService {
     @Transactional
     public QuestionResponseDto save(Long userNo, QuestionSaveRequestDto requestDto) {
         Question toSaveQuestion = Question.builder()
-                                            .user(userRepository.findById(userNo).get())
+                                            .user(userRepository.findById(userNo).orElseThrow(() -> new NoSuchElementException("해당 사용자를 찾을 수 없습니다. USER-NO:"+userNo)))
                                             .content(requestDto.getContent())
                                             .category(QuestionCategory.valueOf(requestDto.getCategoryValue()))
                                             .views(0)
                                             .build();
         Question savedQuestion = questionRepository.save(toSaveQuestion);
         //질문이미지 저장해야됨!!!
-        return new QuestionResponseDto(questionRepository.findById(savedQuestion.getNo()).get());
+        return new QuestionResponseDto(questionRepository.findById(savedQuestion.getNo()).orElseThrow(()->new IllegalStateException("질문 저장이 완료되지 않았습니다.")));
 
     }
 
     @Transactional
     public QuestionResponseDto findByNo(Long no) {
         Question question = questionRepository.findById(no)
-                .orElseThrow(() -> new IllegalArgumentException("해당 질문을 찾을 수 없습니다."));
+                .orElseThrow(() -> new IllegalArgumentException("해당 질문을 찾을 수 없습니다. QUESTION-NO:"+no));
 
         question.plusViews();
 
@@ -51,7 +52,7 @@ public class QuestionService {
     @Transactional
     public QuestionResponseDto update(Long no, QuestionUpdateRequestDto requestDto) {
         Question question = questionRepository.findById(no)
-                .orElseThrow(() -> new IllegalArgumentException("해당 질문을 찾을 수 없습니다."));
+                .orElseThrow(() -> new IllegalArgumentException("해당 질문을 찾을 수 없습니다. QUESTION-NO:"+no));
         question.updateQuestion(requestDto.getContent(), QuestionCategory.valueOf(requestDto.getCategoryValue()));
 
 //        questionImgRepository.deleteByQuestionNo(no);
@@ -62,38 +63,36 @@ public class QuestionService {
 //                        .questionImgUrl(imgUrl)
 //                        .build()));     !!!나중에 주석 제거
 
-        return new QuestionResponseDto(questionRepository.findById(no).get());
+        return new QuestionResponseDto(questionRepository.findById(no).orElseThrow(()->new IllegalStateException("질문 변경이 완료되지 않았습니다. QUESTION-NO"+no)));
     }
 
     @Transactional
     public void delete(Long no) {
         answerRepository.deleteByQuestionNo(no);
         questionRepository.delete(questionRepository.findById(no)
-                .orElseThrow(() -> new IllegalArgumentException("존재하지 않는 질문입니다.")));
+                .orElseThrow(() -> new IllegalArgumentException("해당 질문을 찾을 수 없습니다. QUESTION-NO:"+no)));
 
     }
 
     public List<QuestionResponseDto> findTop3ByUserNo(Long no) {
         List<Question> questionTop3List = questionRepository.findTop3ByUserNoOrderByCreatedDateDesc(no);
-        List<QuestionResponseDto> top3ResponseDtoList = questionTop3List.stream()
-                                                                .map(question -> new QuestionResponseDto(question))
-                                             .collect(Collectors.toList());
-        return top3ResponseDtoList;
+        return questionTop3List.stream()
+                                .map(QuestionResponseDto::new)
+                                .collect(Collectors.toList());
     }
 
     public List<QuestionResponseDto> getTop5ByViews() {
         LocalDateTime twoDaysAgo = LocalDateTime.of(LocalDate.now().minusDays(2), LocalTime.of(0,0,0));
 
         List<Question> questionTop5List = questionRepository.findTop5ByCreatedDateGreaterThanOrderByViewsDesc(twoDaysAgo);
-        List<QuestionResponseDto> top5ResponseDtoList = questionTop5List.stream()
-                                                                .map(question -> new QuestionResponseDto(question))
-                                             .collect(Collectors.toList());
-        return top5ResponseDtoList;
+        return questionTop5List.stream()
+                                .map(QuestionResponseDto::new)
+                                .collect(Collectors.toList());
     }
 
-    public Long getQuestionWrtier(Long no) {
+    public Long getQuestionWriter(Long no) {
         Question question = questionRepository.findById(no)
-                .orElseThrow(() -> new IllegalArgumentException("존재하지 않는 질문입니다."));
+                .orElseThrow(() -> new IllegalArgumentException("해당 질문을 찾을 수 없습니다. QUESTION-NO:"+no));
         return question.getUser().getNo();
     }
 }
